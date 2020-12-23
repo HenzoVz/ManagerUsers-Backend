3a7b3b01fb99cfff69ac79db6fdea6135b7ea507
HenzoVZ <murilohezo@gmail.com>
2020-12-23 15:07:14 -0300

    update and delete registry

--- src/controllers/Registry/CreateRegistryController.ts
+++ src/controllers/Registry/CreateRegistryController.ts
@@ -16,7 +16,7 @@ interface Request extends RequestDTO {
 const registriesRouter = Router();
 registriesRouter.use(ensureAuthenticated);
 
-registriesRouter.get('/', async (request: any, response) => {
+registriesRouter.get('/pagination/', async (request: any, response) => {
   const page = request.query.page || 1;
   const take = request.query.take || 10;
   const skip = (page - 1) * take;
@@ -32,6 +32,34 @@ registriesRouter.get('/', async (request: any, response) => {
   return response.json(registries);
 });
 
+registriesRouter.get('/search/:name', async (request: any, response) => {
+  const page = request.query.page || 1;
+  const take = request.query.take || 10;
+  const skip = (page - 1) * take;
+  const { registry_id } = request.query;
+  const { name } = request.params;
+
+  const registryRepository = getRepository(Registry);
+  const registries = await registryRepository.find({
+    where: { registry_id, name },
+    skip,
+    take,
+  });
+
+  return response.json(registries);
+});
+
+registriesRouter.get('/:id', async (request: any, response) => {
+  const { id } = request.params;
+
+  const registryRepository = getRepository(Registry);
+  const registries = await registryRepository.find({
+    where: { id },
+  });
+
+  return response.json(registries);
+});
+
 registriesRouter.post('/', async (request, response) => {
   const { ...rest }: Request = request.body;
 
@@ -42,6 +70,29 @@ registriesRouter.post('/', async (request, response) => {
   });
 
   return response.json(registry);
+});
+
+registriesRouter.put('/:id', async (request, response) => {
+  const { id } = request.params;
+  const { ...rest }: Request = request.body;
+
+  const registryRepository = getRepository(Registry);
+  await registryRepository.update(id, {
+    ...rest,
+  });
+
+  const updatedRegistry = await registryRepository.find({ id });
+
+  return response.json(updatedRegistry);
+});
+
+registriesRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const registryRepository = getRepository(Registry);
+  const registries = await registryRepository.delete({ id: `${id}` });
+
+  return response.json(registries);
 });
 
 export default registriesRouter;
